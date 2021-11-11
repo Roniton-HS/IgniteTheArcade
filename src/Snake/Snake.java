641c86a1606a6cccf66d083e5071ffe3afaaf732
Seyrila <judige@freenet.de>
2021-11-11 15:13:48 +0100

    GIT

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Snake {
+    public static boolean gameStart = false;
     int x, y;
     int tick;
     int directions;
@@ -27,14 +28,13 @@ public class Snake {
 
     public void tick() {
         move();
-        //die();
-
     }
 
     public void render(Graphics g) {
         for (int i = 0; i < snake.size(); i++) {
             Rectangle rectangle = (Rectangle) snake.get(i);
             g.fillRect(rectangle.getBounds().x, rectangle.getBounds().y, rectangle.getBounds().width, rectangle.getBounds().height);
+            g.setColor(Color.green);
         }
     }
 
@@ -59,61 +59,50 @@ public class Snake {
 
         if (tick > 10) {
             switch (directions) {
-                case 0:
-                    y = y - 32;
-                    break;
-                case 1:
-                    x = x - 32;
-                    break;
-                case 2:
-                    y = y + 32;
-                    break;
-                case 3:
-                    x = x + 32;
-                    break;
+                case 0 -> y = y - 32;
+                case 1 -> x = x - 32;
+                case 2 -> y = y + 32;
+                case 3 -> x = x + 32;
             }
             snake.add(new Rectangle(this.x, this.y, 32, 32));
-
-
-            if (start > 3) {
-
-                if (!appleCollected) {
-                    snake.remove(0);
-
-                } else {
-                    appleCollected = false;
-                }
-
-                Rectangle head = (Rectangle) snake.get(snake.size() - 1);
-
-                for (int i = 0; i < snake.size(); i++) {
-                    Rectangle rectangle = (Rectangle) snake.get(i);
-                    if (head.getBounds().intersects(rectangle.getBounds()) && i != snake.size() -1 ) {
-                        World1 world1 = new World1(game);
-                        Worlds.setWorld(world1);
-                    }
-                }
-
-
-                Rectangle up = new Rectangle(32, 32, 863, 1);
-                Rectangle down = new Rectangle(32, 959, 863, 1);
-                Rectangle left = new Rectangle(32, 32, 1, 863);
-                Rectangle right = new Rectangle(959, 32, 1, 863);
-                if (head.getBounds().intersects(up.getBounds()) ||
-                        head.getBounds().intersects(down.getBounds()) ||
-                        head.getBounds().intersects(left.getBounds()) ||
-                        head.getBounds().intersects(right.getBounds())) {
-
-                    World1 world1 = new World1(game);
-                    Worlds.setWorld(world1);
-                }
-
-            }
             tick = 0;
-            start++;
-
+            waitForStart();
         } else {
             tick++;
+        }
+    }
+
+    public void waitForStart() {
+        if (start > 3) {
+            if (!appleCollected) {
+                snake.remove(0);
+            } else {
+                appleCollected = false;
+            }
+            die();
+        }
+
+        start++;
+    }
+
+    public void die() {
+        Rectangle head = (Rectangle) snake.get(snake.size() - 1);
+
+        for (int i = 0; i < snake.size(); i++) {
+            Rectangle rectangle = (Rectangle) snake.get(i);
+            if (head.getBounds().intersects(rectangle.getBounds()) && i != snake.size() - 1) {
+                gameStart = false;
+            }
+        }
+        Rectangle up = new Rectangle(32, 32, 863, 1);
+        Rectangle down = new Rectangle(32, 959, 863, 1);
+        Rectangle left = new Rectangle(32, 32, 1, 863);
+        Rectangle right = new Rectangle(959, 32, 1, 863);
+        if (head.getBounds().intersects(up.getBounds()) ||
+                head.getBounds().intersects(down.getBounds()) ||
+                head.getBounds().intersects(left.getBounds()) ||
+                head.getBounds().intersects(right.getBounds())) {
+            gameStart = false;
         }
     }
 
