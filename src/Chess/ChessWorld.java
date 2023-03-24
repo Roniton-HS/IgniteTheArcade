f3cef62cefce6740d31288a3fa78ae449273a981
Seyrila <judige@freenet.de>
2023-03-24 19:31:48 +0100

    die anfänge und so

--- src/Chess/ChessWorld.java
+++ src/Chess/ChessWorld.java
@@ -7,6 +7,11 @@ import java.awt.*;
 
 public class ChessWorld extends Worlds {
 
+    int[][] figuresSave = new int[8][8];
+
+    int fieldSize = 100;
+    boolean white = true;
+
     /**
      * Constructor
      *
@@ -24,6 +29,22 @@ public class ChessWorld extends Worlds {
 
     @Override
     public void render(Graphics g){
+        renderField(fieldSize, g);
 
+    }
+
+    public void renderField(int fieldSize, Graphics g){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(white){
+                    g.setColor(Color.WHITE);
+                    white = !white;
+                } else {
+                    g.setColor(Color.GRAY);
+                    white = !white;
+                }
+                g.fillRect(100+(j*fieldSize),100+(i*fieldSize), fieldSize, fieldSize);
+            }
+        }
     }
 }
