f6829adfc4ea522b51d8187723be2815f4fd81d7
Duwutape <quinn.langheld@gmx.de>
2023-03-27 02:18:03 +0200

    arkanoid: added borders and player

--- src/Arkanoid/Arkanoid.java
+++ src/Arkanoid/Arkanoid.java
@@ -1,6 +1,5 @@
 package Arkanoid;
 
-import Main.Constants;
 import Main.Game;
 import Worlds.Worlds;
 
@@ -8,49 +7,62 @@ import java.awt.*;
 
 public class Arkanoid extends Worlds {
 
+    final int WINDOW_WIDTH = 500;
+    final int WINDOW_HEIGHT = 1000;
     final int PLAYER_SPEED = 5;
-    Rectangle player = new Rectangle(500, 500, 50, 50);
-    Rectangle collision = new Rectangle(player.x - 10, player.y, player.width + (2*10), player.height);
-    Rectangle obj = new Rectangle(100, 503, 50, 44);
+    Rectangle player = new Rectangle(200, 900, 100, 10);
+    Rectangle collision = new Rectangle(player.x - PLAYER_SPEED, player.y, player.width + (2 * PLAYER_SPEED), player.height);
+    Rectangle borderL = new Rectangle(40, 50, 10, 900);
+    Rectangle borderR = new Rectangle(450, 50, 10, 900);
+    Rectangle borderT = new Rectangle(50, 40, 400, 10);
+    Rectangle borderB = new Rectangle(50, 950, 400, 10);
 
     /**
      * Constructor
      */
     public Arkanoid(Game game) {
         super(game);
-        game.getDisplay().resize(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
+        game.getDisplay().resize(WINDOW_WIDTH+16, WINDOW_HEIGHT+39);
     }
 
     @Override
     public void tick() {
 
         if (game.getKeyHandler().a) {
-            if (player.getBounds().intersects(obj.getBounds())) {
-                //System.out.println(obj.x + obj.width);
-                player.x = obj.x + obj.width + 1;
-                collision.x = player.x-10;
+            if (collision.getBounds().intersects(borderL.getBounds())) {
+                player.x = borderL.x + borderL.width;
+                collision.x = player.x - PLAYER_SPEED;
+            } else {
+                player.x -= PLAYER_SPEED;
+                collision.x -= PLAYER_SPEED;
             }
-            player.x -= PLAYER_SPEED;
-            collision.x -= PLAYER_SPEED;
         }
         if (game.getKeyHandler().d) {
-            player.x += PLAYER_SPEED;
-            collision.x += PLAYER_SPEED;
-        }
-
-        if (collision.getBounds().intersects(obj.getBounds())) {
-            System.out.println("Collision");
+            if (collision.getBounds().intersects(borderR.getBounds())) {
+                player.x = borderR.x - player.width;
+                collision.x = player.x - PLAYER_SPEED;
+            } else {
+                player.x += PLAYER_SPEED;
+                collision.x += PLAYER_SPEED;
+            }
         }
     }
 
     @Override
     public void render(Graphics g) {
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        g.setColor(Color.GRAY);
+        g.fillRect(50, 50, WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100);
+
+
         g.setColor(Color.blue);
         g.fillRect(player.x, player.y, player.width, player.height);
         //g.fillOval(500,500,200,200);
         g.setColor(Color.red);
-        g.fillRect(obj.x, obj.y, obj.width, obj.height);
-        g.setColor(Color.GRAY);
-        g.drawRect(collision.x, collision.y, collision.width, collision.height);
+        g.fillRect(borderL.x, borderL.y, borderL.width, borderL.height);
+        g.fillRect(borderR.x, borderR.y, borderR.width, borderR.height);
+        g.fillRect(borderT.x, borderT.y, borderT.width, borderT.height);
+        g.fillRect(borderB.x, borderB.y, borderB.width, borderB.height);
     }
 }
