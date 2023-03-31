cfb11c9a96f7b045fa5f230dffcffa844085b811
Seyrila <judige@freenet.de>
2023-03-31 18:23:30 +0200

    bullshit chess

--- src/Chess/Bishop.java
+++ src/Chess/Bishop.java
@@ -14,4 +14,32 @@ public class Bishop extends Figures {
             image = ImageLoader.loadImage("/chess/White/BishopWhite.png");
         }
     }
+
+    public void tick() {
+        whereTo();
+        moveFigure();
+    }
+
+    public void whereTo() {
+        coordinates.clear();
+
+        /*
+        Moves for black rooks
+         */
+        if (black) {
+            for (int i = y + 1; i < 9; i++) { //down left
+                for (int j = x - 1; j > 0; j--) {
+                    if (figuresSave[j][i] == null || !figuresSave[j][i].black) {
+                        coordinates.add(new Coordinates(j, i));
+                        if (figuresSave[j][i] != null) {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
 }
