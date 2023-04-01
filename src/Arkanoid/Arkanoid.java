eeeff973fe8279c1fcdc638df73c7a5a1e60cebf
Duwutape <quinn.langheld@gmx.de>
2023-04-01 03:27:41 +0200

    arkanoid: removed unused code

--- src/Arkanoid/Arkanoid.java
+++ src/Arkanoid/Arkanoid.java
@@ -64,13 +64,8 @@ public class Arkanoid extends Worlds {
 
     private void createBricks() {
         Random random = new Random();
-
-        //int index = random.nextInt(0, getPatterns().size());
-        //bricks = getPatterns().get(index);
-
         index = random.nextInt(pattern.getPatterns().size());
         bricks = pattern.getPatterns().get(index);
-
     }
 
     @Override
